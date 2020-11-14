3233e6cc571e213ad49e8e498b4343d6254ff0b2
WeizhengJ <70171753+WeizhengJ@users.noreply.github.com>
2020-11-14 16:59:33 -0600

    Create htable.cc

---
+++ htable.cc
@@ -0,0 +1,24 @@
+#include "htable.h"
+
+#define N1 10  // num of decimal digits
+#define N2 26  // num of upper-case letters
+
+// Hash function, you may NOT modify this function
+int HT::hash(const string &s) {
+  int n = (N2 * N1) * (s[0] - 'A') + N1 * (s[1] - 'A') + (s[2] - '0');
+
+  return n % hsize;
+}
+
+// You will complete the code below ...
+HT::HT(const unsigned &hs) {}
+
+HT::~HT() {}
+
+void HT::insert(const Entry &e) {}
+
+void HT::search(const string &s) {}
+
+void HT::hTable_print() {}
+
+void HT::pTable_print() {}
